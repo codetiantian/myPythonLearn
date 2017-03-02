e59b193c33cf9bd640afe41b29e41f138e1947ba
崔兵兵 <1655902151@qq.com>
2017-03-02 16:35:08 +0800

    学习Python第一天，学习了随机数，while 、if else 的用法  、引入外援(import)

---
+++ PythonLearn-01.py
@@ -0,0 +1,26 @@
+import random
+
+print('-----------PythonLearn--------------')
+
+secret = random.randint(1, 100)
+
+temp = input("请输入你猜想的数字:")
+guess = int(temp)
+
+if guess == secret :
+    print("恭喜，猜正确")
+
+else :
+    while guess != secret :
+        if guess == secret :
+            print("恭喜，猜正确")
+        else :
+            if guess > secret :
+                print("大了")
+            else :
+                print("小了")
+                
+        temp = input("请再次输入你猜想的数字:")
+        guess = int(temp)
+
+print("游戏结束，谢谢参与")
